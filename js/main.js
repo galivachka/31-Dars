eca03aa7ec589b9977229f4deecc8c953e20c2e0
galivachka <@muhammadusmonov182@gmail.com>
2023-03-05 19:29:32 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// const form = document.querySelector('.form'),
+//     input = document.querySelector('.input'),
+//     select = document.querySelector(".select")
+// optionDollar = document.querySelector('.dollar'),
+// optionRubl = document.querySelector('.rubl'),
+// optionYuan = document.querySelector('.yuan'),
+// optionDinor = document.querySelector('.dinor'),
+// optionFunt = document.querySelector('.funt'),
+
+// formBtn = document.querySelector('.btn'),
+//     text = document.querySelector(".text");
+
+// input.value = Number(input.value)
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     if (select.value === "dollar") {
+
+//         text.textContent = `${input.value * 11000} so'mda`
+
+//     } else if (select.value === "rubl") {
+
+//         text.textContent = `${input.value * 69.38} so'mda`
+//     }
+//     if (select.value === "yuan") {
+
+//         text.textContent = `${input.value * 1.646} so'mda`
+//     } else if (select.value === "dinor") {
+
+//         text.textContent = `${input.value * 37.034} so'mda`
+//     }
+//     if (select.value === "funt") {
+
+//         text.textContent = `${input.value * 13.639} so'mda`
+//     } else {
+//         console.log("number kiriting");
+//         text.textContent = "";
+//     }
+
+//  })
+
+
+// 2 Uy Ishi
+
+// const inpt = document.querySelector(".inpt"),
+//     btn = document.querySelector(".btn"),
+//     form = document.querySelector(".form");
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+// })
+// inpt.value = Number(inpt.value)
+
+// switch (inpt.value) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+
+//     case 2:
+//         console.log("Tuseday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thurday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Unday Yoq Anqov");
+//         break;
+// }
+// console.log(inpt.value);
+
+// 3-1 Uy Ishi
+
+// const pattern = /^\d{5}-\d{3}-\d{2}-\d{2}/;
+
+// const myNumber = '99893-630-00-29';
+
+// console.log(pattern.test(myNumber));
+
+// 3-2 Uy Ishi
