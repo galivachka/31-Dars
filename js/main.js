761706e18d1ac19a2cc4ecfae7df4e082d0a2a12
galivachka <@muhammadusmonov182@gmail.com>
2023-03-06 18:13:49 +0500

    dada

--- js/main.js
+++ js/main.js
@@ -43,44 +43,58 @@
 
 // 2 Uy Ishi
 
-// const inpt = document.querySelector(".inpt"),
+// const kun = document.querySelector(".kun"),
 //     btn = document.querySelector(".btn"),
-//     form = document.querySelector(".form");
+//     form = document.querySelector(".form"),
+//     kun__text = document.querySelector(".kun__text")
 
 // form.addEventListener('submit', (e) => {
 //     e.preventDefault();
+//     switch (kun.value) {
+//         case '1':
+
+//             console.log("Monday");
+//             kun__text.textContent = "Monday";
+//             break;
+
+//         case "2":
+//             console.log("Tuseday");
+//             kun__text.textContent = "Tuesday";
+//             break;
+//         case "3":
+//             console.log("Wednesday");
+//             kun__text.textContent = "Wednasday";
+//             break;
+//         case "4":
+//             console.log("Thurday");
+//             kun__text.textContent = "Thurday";
+//             break;
+//         case "5":
+//             console.log("friday");
+//             kun__text.textContent = "Friday";
+//             break;
+//         case "6":
+//             console.log("Saturday");
+//             kun__text.textContent = "Saturday";
+//             break;
+//         case "7":
+//             console.log("Sunday");
+//             kun__text.textContent = "Sunday";
+//             break;
+
+//         default:
+//             console.log("Unday Yoq Anqov");
+//             kun__text.textContent = "Bunday Yoq";
+//             break;
+//     }
 // })
-// inpt.value = Number(inpt.value)
 
-// switch (inpt.value) {
-//     case 1:
-//         console.log("Monday");
-//         break;
 
-//     case 2:
-//         console.log("Tuseday");
-//         break;
-//     case 3:
-//         console.log("Wednesday");
-//         break;
-//     case 4:
-//         console.log("Thurday");
-//         break;
-//     case 5:
-//         console.log("friday");
-//         break;
-//     case 6:
-//         console.log("Saturday");
-//         break;
-//     case 7:
-//         console.log("Sunday");
-//         break;
 
-//     default:
-//         console.log("Unday Yoq Anqov");
-//         break;
-// }
-// console.log(inpt.value);
+// console.log(kun.value);
+
+
+
 
 // 3-1 Uy Ishi
 
@@ -91,3 +105,25 @@
 // console.log(pattern.test(myNumber));
 
 // 3-2 Uy Ishi
+const form = document.querySelector(".form");
+const pattern4 = /^\d+$/;
+const passwordInp1 = document.querySelector(".passwordInp1");
+const passwordInp2 = document.querySelector(".passwordInp2");
+const summ1 = passwordInp1.match(pattern4);
+const summ2 = passwordInp2.match(pattern4);
+
+passwordInp1.value = Number(passwordInp1);
+passwordInp2.value = Number(passwordInp2);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+    if (passwordInp1.value == passwordInp2.value) {
+        console.log("Tasdiqlandi");
+    } else if (passwordInp1.value > passwordInp2.value) {
+        console.log("Ikkinchi Paroliz notog'ri");
+    } else {
+        console.log("Parol Notog'ri");
+    }
+})
